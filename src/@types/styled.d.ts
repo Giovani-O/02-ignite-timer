1936186d2f95632c4a20d2961c03584629c98606
Giovani-O <giovani.o@outlook.com.br>
2022-10-26 15:07:05 -0300

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+// Type definition file
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
