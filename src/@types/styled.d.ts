f1e185f278eeb63fee86487e5cea45762bb6e53a
Giovani-O <giovani.o@outlook.com.br>
2022-10-28 17:25:44 -0300

    feat: eslint configuration

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,8 @@
 // Type definition file
-import 'styled-components';
+import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
-      <Button variant="secondary"/>
-      <Button variant="success"/>
+      <Button variant="secondary" />
+      <Button variant="success" />
       <Button />
 
       <GlobalStyle />
